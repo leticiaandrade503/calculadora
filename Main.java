a869ae03c1597bea488e12e7c0c2d6a845036e75
leticiaandrade503 <leticia.11847813@aluno.mg.gov.br>
2026-06-01 14:03:42 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+void main() {
+
+
+    Scanner sc= new Scanner (system.in)
+
+    System.out.println(CALCULADORA);
+    System.out.println("1 - Soma");
+    System.out.println("2 - Subtração");
+    System.out.println("3 - Multiplicação");
+    System.out.println("4 - Divisão");
+    System.out.println("0 -Potencia)
+
+
+            opcão= scanner.nextint
+            System.out.println("Saindo da calculadora. Até mais!");
+
+    //Entrada de dados
+
+    if(opção >4) {
+        system.out.printl
+
+        case 1
+            System.out.printl (Digite o divisor)
+
+            num2 = scanner.nextDouble ();
+        case 2
+            System.ou.printl ("digite a base:)
+                    scanner.nextdouble();
+
+
+            double base = 2.0;
+            double expoente = 3.0; // Potência N
+
+            // Executa o cálculo da potência
+            double resultado = Math.pow(base, expoente);
+
+            // Exibe o resultado (2 elevado a 3 = 8)
+            System.out.println(base + " elevado a " + expoente + " é igual a " + resultado);
+    }
+             System.out.printl (saindo de programa.Até logo!0
